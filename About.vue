d6b7ee7c93137ad54c59589c1b10c4aef8eb91c3
Muskan Sachan <104751681+Muskan1113@users.noreply.github.com>
2024-05-12 17:08:14 +0530

    Add files via upload

---
+++ About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <label class="about-label"
+      >GitHub:
+      <a href="https://github.com/Muskan1113/website-tracking-app" target="_blank"
+        >github.com/Muskan1113/website-tracking-app</a
+      ></label
+    >
+    
+   
+
+  </div>
+  <MyApps />
+</template>
+
+<script lang="ts">
+export default {
+  name: 'About',
+};
+</script>
+
+
+
+<style scoped>
+.about .about-label {
+  font-size: 30pxpx;
+  margin-bottom: 30px;
+  display: block;
+}
+</style>
